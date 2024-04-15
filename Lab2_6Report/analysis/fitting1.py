7f3b88df184edd774541f7a68045e3c4d7ceebfb
TaLEsCuber <daiph0114@gmail.com>
2024-04-15 23:35:15 +0800

    finish pre part of 6th report

---
+++ Lab2_6Report/analysis/fitting1.py
@@ -0,0 +1,36 @@
+import math
+
+import pandas as pd
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+
+# 读取Excel文件中的数据
+df = pd.read_excel('DataAnalysis.xlsx')
+
+# 提取需要拟合的数据列
+# 将x_data和y_data转换为numpy数组
+x_data = np.array(df['f'])
+y_data = np.array(df['振动幅度/mm'])
+
+
+# 定义要拟合的函数模型
+def func(w, F, m, w0, beta):
+    return F/(m*np.sqrt((w0**2-w**2)**2+4*beta**2*w**2))
+
+# 使用curve_fit进行拟合
+popt, pcov = curve_fit(func, x_data, y_data)
+
+# 输出拟合参数
+print("拟合参数:", popt)
+
+x_plot = np.linspace(503.5, 504.5, num=1000)
+
+# 绘制拟合结果
+plt.figure()
+plt.scatter(x_data, y_data, label='Data')
+plt.plot(x_plot, func(x_plot, *popt), 'r-', label='Fit: a=%5.3f, b=%5.3f, w0=%5.3f, beta=%5.3f' % tuple(popt))
+
+
+plt.legend()
+plt.show()
